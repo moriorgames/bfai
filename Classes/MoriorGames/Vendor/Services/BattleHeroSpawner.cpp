f3dedfeb847751f1872cdcef144eb77bc501b772
Morior <moriorgames@gmail.com>
2018-05-17 09:49:05 +0200

    Fix bug when spawning hero and do not assign to user has performed the action

--- Classes/MoriorGames/Vendor/Services/BattleHeroSpawner.cpp
+++ Classes/MoriorGames/Vendor/Services/BattleHeroSpawner.cpp
@@ -11,6 +11,7 @@ void BattleHeroSpawner::spawn(Skill *skill, BattleHero *battleHero, BattleAction
     auto spawnHero = new BattleHero;
     spawnHero->setId(skill->getExtra());
     spawnHero->setSide(battleHero->getSide());
+    spawnHero->setUserToken(battleAction->getUserToken());
     spawnHero->setBattleHeroId(battle->getBattleHeroes().size() + 1);
     auto coordinate = grid->findByXY(battleAction->getCoordinate()->x, battleAction->getCoordinate()->y);
     coordinate->occupied = true;
